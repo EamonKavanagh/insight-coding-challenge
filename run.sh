2b58547ec9d7affee6f31b470266666ea7eb1adc
Eamon Kavanagh <kavanagh.c.eamon@gmail.com>
2016-04-03 19:38:29 -0400

    Added dependency downloading

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-
+wget "http://central.maven.org/maven2/com/google/code/gson/gson/2.6.2/gson-2.6.2.jar" -P ./src/
 
 javac -cp ./src/gson-2.6.2.jar ./src/Tweet.java ./src/HashtagGraph.java ./src/Runner.java
 
