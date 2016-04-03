937d66afba005bbe5d49a72745d76c5ef02a3fa0
Eamon Kavanagh <kavanagh.c.eamon@gmail.com>
2016-04-03 19:52:38 -0400

    Updated run.sh to only download dependency if missing

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-wget "http://central.maven.org/maven2/com/google/code/gson/gson/2.6.2/gson-2.6.2.jar" -P ./src/
+if [ ! -f ./src/gson-2.6.2.jar ]; then
+	echo "Downloading dependency: gson-2.6.2.jar"
+	curl -o ./src/gson-2.6.2.jar "http://central.maven.org/maven2/com/google/code/gson/gson/2.6.2/gson-2.6.2.jar"
+fi
 
 javac -cp ./src/gson-2.6.2.jar ./src/Tweet.java ./src/HashtagGraph.java ./src/Runner.java
 
