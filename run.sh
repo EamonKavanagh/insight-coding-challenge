16c9e57a3b4db4573a10961ae10ccf4254f10fbe
Eamon Kavanagh <kavanagh.c.eamon@gmail.com>
2016-04-03 20:14:26 -0400

    Fixed typos in run script

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 if [ ! -f ./src/gson-2.6.2.jar ]; then
-	printf "Downloading dependency: gson-2.6.2.jar\n"
+	printf "Downloading dependency: gson-2.6.2.jar\n\n"
 	curl -o ./src/gson-2.6.2.jar "http://central.maven.org/maven2/com/google/code/gson/gson/2.6.2/gson-2.6.2.jar"
-	printf "Finished downloading\n"
+	printf "Finished downloading\n\n"
 fi
 
-printf "Compiling code...\n\n"
+printf "Compiling code\n\n"
 javac -cp ./src/gson-2.6.2.jar ./src/Tweet.java ./src/HashtagGraph.java ./src/Runner.java
 
-printf "Running code with tweets from tweet_input/tweets.txt...\n"
+printf "Running code with tweets from tweet_input/tweets.txt\n\n"
 java -cp ./src/gson-2.6.2.jar:./src Runner ./tweet_input/tweets.txt ./tweet_output/output.txt
+
+printf "Finished!\n"
